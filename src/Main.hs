1b62fa5d27a8f557ade7c2f22bb4685df96e61de
Francesco Montesano <franz.bergesund@gmail.com>
2015-01-27 22:24:09 +0100

    Haskeline support added

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import System.IO
+import System.Console.Haskeline
+import Control.Monad.IO.Class
 
 import Commands (commandMap)
 import Types
@@ -19,24 +21,26 @@ errorstr = init $ unlines
 
 -- Function that parses the command and call the appropriate functions according
 -- to the request
-parse :: EStatus -> String -> IO EStatus
-parse status string = if null string
-                      then return status
-                      else case Map.lookup (stringHead command) commandMap of
-                           Just f -> f status options
-                           Nothing -> putStrLn errorstr >> return status
-                           where (command, options) = splitAt 1 $ words string
+parse :: EStatus -> Maybe String -> IO EStatus
+parse status Nothing = return status
+parse status (Just string) =
+                  case Map.lookup (stringHead command) commandMap of
+                  Just f -> f status options
+                  Nothing -> putStrLn errorstr >> return status
+                  where (command, options) = splitAt 1 $ words string
                           
 
 welcome = unlines ["Welcome!"
                   ,"For a list of available commands type 'help'" 
                   ,"To exit type 'exit'"]
 
+
+command :: EStatus -> InputT IO ()
+command status = getInputLine "~> " >>= command'
+                 where
+                   command' :: Maybe String -> InputT IO ()
+                   command' mstr = liftIO (parse status mstr) >>= command
+
+               
 main = putStr welcome >>
-       return (toEStatus Map.empty) >>=
-       command 
-       where command status = putStr "~> " >>
-                              hFlush stdout >>
-                              getLine >>= 
-                              (parse status) >>=
-                              command
+       runInputT defaultSettings (command (toEStatus Map.empty))
